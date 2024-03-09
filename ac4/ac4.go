d87ed6884407e0ba1cabcb0f7c5a7947444e86a0
LiporaceM <108703224+LiporaceM@users.noreply.github.com>
2024-03-09 20:05:58 -0300

    -

--- ac4/ac4.go
+++ ac4/ac4.go
@@ -11,18 +11,26 @@ func hanoi(n int, origem, trabalho, destino string) {
 		hanoi(n-1, trabalho, origem, destino)
 	}
 }
-func findPair(arr []int, target int) (int, int) {
-	seen := make(map[int]bool)
-	for _, num := range arr {
-		complement := target - num
+func findPair(arr []int, target int) (int, int, bool) {
+	left, right := 0, len(arr)-1
 
-		if seen[complement] {
-			return complement, num
+	for left < right {
+		sum := arr[left] + arr[right]
+
+		if sum == target {
+			// Encontrou um par que soma para o alvo
+			return arr[left], arr[right], true
+		} else if sum < target {
+			// Se a soma for menor que o alvo, mova o ponteiro esquerdo para a direita
+			left++
+		} else {
+			// Se a soma for maior que o alvo, mova o ponteiro direito para a esquerda
+			right--
 		}
-
-		seen[num] = true
 	}
-	return -1, -1
+
+	// Nenhum par encontrado, retorna (-1, -1) e false
+	return -1, -1, false
 }
 
 func main() {
@@ -32,13 +40,13 @@ func main() {
 	fmt.Println(("----------------------------------"))
 
 	array := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	target := 5
+	target := 10
 
-	num1, num2 := findPair(array, target)
+	num1, num2, found := findPair(array, target)
 
-	if num1 == -1 && num2 == -1 {
-		fmt.Println("(-1,-1)")
-	} else {
+	if found {
 		fmt.Printf("Par encontrado: (%d,%d)\n", num1, num2)
+	} else {
+		fmt.Println("(-1-1)")
 	}
 }
